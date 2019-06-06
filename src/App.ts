d48ec751a736850812d24ab3afeb3bd88cde8988
Skyler Riley <riley.skyler@gmail.com>
2019-06-06 16:29:06 -0700

    begin

---
+++ src/App.ts
@@ -0,0 +1,15 @@
+import * as Dotenv from 'dotenv';
+const express = require('express');
+
+const app = express();
+
+app.post('/voice', (req: any, res: any) => {
+    
+});
+
+app.post('/sms', (req: any, res: any) => {
+
+});
+
+
+app.listen(1337);
